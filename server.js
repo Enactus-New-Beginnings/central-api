3f6ac57de4fc2f5c7d421e572e18be75dc125147
Franklin Yin <yin4thewin@gmail.com>
2021-11-18 00:46:39 +0000

    Add cors

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 8000
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,          
+   optionSuccessStatus:200,
+}
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://newbeginnings-7fed9-default-rtdb.firebaseio.com"
@@ -11,6 +17,7 @@ admin.initializeApp({
 const resourceRoute = require('./routes/resources')
 const usersRoute = require('./routes/users')
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/auth', usersRoute)
 app.use('/resources', resourceRoute)

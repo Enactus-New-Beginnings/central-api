72fa5a33e40a3b8cb1edf1e0007e6e8a9347dd07
Franklin Yin <yin4thewin@gmail.com>
2022-08-27 19:44:49 -0400

    Migrate to Firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({origin: true}));
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://newbeginnings-7fed9-default-rtdb.firebaseio.com",
+});
+
+const resourceRoute = require("./routes/resources");
+const usersRoute = require("./routes/users");
+
+app.use(express.json());
+app.use("/auth", usersRoute);
+app.use("/resources", resourceRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+exports.widgets = functions.https.onRequest(app);
--- functions/routes/users.js
+++ functions/routes/users.js
@@ -1,22 +1,22 @@
-const router = require('express').Router()
+const router = require("express").Router();
 const admin = require("firebase-admin");
-const auth=admin.auth()
-const db=admin.database()
+const auth=admin.auth();
+const db=admin.database();
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
     auth.createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
         var user = userCredential.user;
         db.ref("users/"+user.uid).set({
             email: user.email
-        })
+        });
     })
     .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorCode, errorMessage)
-        res.status(400).json(errorMessage)
+        console.log(errorCode, errorMessage);
+        res.status(400).json(errorMessage);
     });
-})
+});
 
-module.exports = router
+module.exports = router;

54df99de5eea3ad8148ba0e6eee26e46d1c177db
Franklin Yin <yin4thewin@gmail.com>
2022-09-15 18:55:03 -0400

    Add function to update database + authentication

--- functions/routes/resources.js
+++ functions/routes/resources.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const admin = require("firebase-admin");
+const fetch = require("node-fetch");
 const db=admin.database();
 
 router.get("/:resourceName", async (req, res) => {
@@ -15,5 +16,26 @@ router.get("/:resourceName", async (req, res) => {
         res.status(500).json(error);
       });
 });
+
+router.post("/:key/:resourceName", async(req, res)=>{
+  db.ref().child("key").get().then((key)=>{
+    if(req.params.key===key.val()){
+      const resourceLink="https://opensheet.elk.sh/1YCuqvWiWK6ENuXnSFEZU1bpk1HDdWGqP0D_cCm8GSFk/"+req.params.resourceName
+      const response = fetch(resourceLink).then((res)=>{
+        return res.json()
+      }).then(data=>{
+        if(data){
+          db.ref().child("resources").child(req.params.resourceName).set(data).then(()=>{
+            return res.status(200).json(data)
+          })
+        } else{
+          return res.status(500).json("Could not get resource");
+        }
+      });
+    } else{
+      res.status(500).json("not authenticated")
+    }
+  })
+})
 
 module.exports = router;
